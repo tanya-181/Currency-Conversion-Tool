310fe043b73a20f8c2b71b9e977346cc2b77de53
Tanya <157242062+tanya-181@users.noreply.github.com>
2026-09-16 01:49:37 +0530

    Fix API URL for currency conversion
    Updated API URL to use a generic 'api-key' placeholder.

--- currency_conversion_tool.py
+++ currency_conversion_tool.py
@@ -16,7 +16,7 @@ def get_conversion_factor(base_currency: str, target_currency: str) -> float:
     """
     This function fetches the currency conversion factor between a given base currency and a target currency
     """
-    url = f'https://v6.exchangerate-api.com/v6/6406415841ae46dca8061b90/pair/{base_currency}/{target_currency}'
+    url = f'https://v6.exchangerate-api.com/v6/api-key/pair/{base_currency}/{target_currency}'
 
     response = requests.get(url)
 
